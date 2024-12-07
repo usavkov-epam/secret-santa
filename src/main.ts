412cf5c745f5e627167d1913fc3ed43d71e907be
Yury Saukou <yury_saukou@epam.com>
2024-12-07 14:31:29 +0400

    setup webhook for bot on prod env

--- src/main.ts
+++ src/main.ts
@@ -5,18 +5,32 @@ import { Telegraf } from 'telegraf';
 import { connectToDatabase } from './config/database';
 import registerCommands from './commands';
 
-const bot = new Telegraf(process.env.TG_BOT_API_TOKEN!);
+const isProduction = process.env.NODE_ENV === 'production';
+const TELEGRAM_WEBHOOK_URL = process.env.TELEGRAM_WEBHOOK_URL!;
+const TG_BOT_API_TOKEN = process.env.TG_BOT_API_TOKEN!;
+
+const bot = new Telegraf(TG_BOT_API_TOKEN);
 
 registerCommands(bot);
 
-connectToDatabase()
-  .then(async (mongoose) => {
-    await bot.launch(() => console.log('✅ Bot launched'))
-      .catch((err) => {
-        console.error('❌ An error during bot launch:', err);
-        mongoose.disconnect();
-      });
-  });
+connectToDatabase().then(async (mongoose) => {
+  await bot
+    .launch(
+      isProduction
+        ? {
+            webhook: {
+              domain: TELEGRAM_WEBHOOK_URL,
+              port: Number(process.env.PORT || '3000'),
+            },
+          }
+        : {},
+      () => console.log('✅ Bot launched'),
+    )
+    .catch((err) => {
+      console.error('❌ An error during bot launch:', err);
+      mongoose.disconnect();
+    });
+});
 
 process.once('SIGINT', () => bot.stop('SIGINT'));
 process.once('SIGTERM', () => bot.stop('SIGTERM'));
